b68e95bfd64cf5e63dd9b98deceb6da31849d2a7
KGameil <141161917+KGameil@users.noreply.github.com>
2025-06-25 20:41:20 +0300

    made the connection and handlers accessible for child scopes

--- Runtime/BalootApi/BalootLifetimeScope.cs
+++ Runtime/BalootApi/BalootLifetimeScope.cs
@@ -9,9 +9,10 @@ using VContainer.Unity;
 [DefaultExecutionOrder(-100)]
 public class BalootLifetimeScope : LifetimeScope
 {
-    [SerializeField] private ApiConfigSO _apiConfig;
-    [SerializeField] private WebSocketConnection _webSocketConnection;
-    [SerializeField] private LobbySocketHandler _lobbySocketHandler;
+    [SerializeField] protected ApiConfigSO _apiConfig;
+    [SerializeField] protected WebSocketConnection _webSocketConnection;
+    [SerializeField] protected LobbySocketHandler _lobbySocketHandler;
+    [SerializeField] protected ChatSocketHandler _chatSocketHandler;
     // [SerializeField] private ChatSocketHandler _chatSocketHandler;
     public new static IObjectResolver Resolver;
     protected override void Awake()
@@ -28,16 +29,6 @@ public class BalootLifetimeScope : LifetimeScope
         builder.Register<IApiRequest, ApiRequest>(Lifetime.Singleton);
         builder.Register<IAuthService, AuthService>(Lifetime.Singleton);
         builder.Register<ApiFacade>(Lifetime.Singleton);
-        builder.RegisterInstance(_webSocketConnection);
-
-        if (_lobbySocketHandler)
-        {
-            builder.RegisterInstance(_lobbySocketHandler);
-        }
-        // if (_chatSocketHandler)
-        // {
-            // builder.RegisterInstance(_chatSocketHandler);
-        // }
         MapperLayer.InitializeMappers();
     }
 }
