0f5fea9da653e5f3e5030a701b96172f6d617841
Wessam <docstarvingwild@gmail.com>
2025-06-25 14:19:17 +0300

    added serializable attribute to socket data

--- Runtime/ApiConfigSO.cs
+++ Runtime/ApiConfigSO.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ApiHandling.Runtime
@@ -11,6 +12,7 @@ namespace ApiHandling.Runtime
         public List<SocketData> SocketUris;
     }
 
+    [Serializable]
     public class SocketData
     {
         public string SocketName;
