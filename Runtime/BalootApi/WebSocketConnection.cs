ed3b6e2148d7b05164c659486ae25601ed413563
Wessam <docstarvingwild@gmail.com>
2025-07-15 14:19:25 +0300

    trying out webgl shit

--- Runtime/BalootApi/WebSocketConnection.cs
+++ Runtime/BalootApi/WebSocketConnection.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using ApiHandling.Runtime;
+using UnityEngine;
+#if UNITY_WEBGL && !UNITY_EDITOR
+using System.Runtime.InteropServices;
+#endif
 using Cysharp.Threading.Tasks;
 using SocketIOClient;
-using SocketIOClient.Newtonsoft.Json;
-using UnityEngine;
+using ApiHandling.Runtime;
 using VContainer;
+using SocketIOClient.Newtonsoft.Json;
 
 namespace BalootApi
 {
@@ -15,129 +18,299 @@ namespace BalootApi
         #region VAR
         [Inject] private ApiConfigSO _configSo;
         [SerializeField] private List<SocketQueries> _socketQueries;
-        private Dictionary<string, SocketIOUnity> _webSocket=new();
+    #if UNITY_WEBGL && !UNITY_EDITOR
+        // WebGL: maintain a map of socket names to JS instance IDs
+        private Dictionary<string, int> _webGLSockets = new Dictionary<string, int>();
+    #else
+        // Desktop/Mobile: use SocketIOUnity client
+        private Dictionary<string, SocketIOUnity> _webSocket = new Dictionary<string, SocketIOUnity>();
+    #endif
+
         public event Action OnSocketConnected;
+
+        // Reconnection settings
+        [Header("Reconnection Settings")]
+        [SerializeField] private bool _enableAutoReconnect = true;
+        [SerializeField] private int _maxReconnectAttempts = 10;
+        [SerializeField] private float _reconnectDelay = 1f;
+        [SerializeField] private float _maxReconnectDelay = 30f;
+
+        // Connection state tracking
+        private Dictionary<string, bool> _isConnected = new Dictionary<string, bool>();
+        private Dictionary<string, int> _reconnectAttempts = new Dictionary<string, int>();
+        private bool _isApplicationFocused = true;
         #endregion
+
         #region ENGINE
 
         public void Connect()
         {
-            foreach (var socketKvp in _webSocket)
+    #if UNITY_WEBGL && !UNITY_EDITOR
+            // Disconnect existing WebGL sockets
+            foreach (var kvp in _webGLSockets)
             {
-                socketKvp.Value?.Disconnect();
+                WebGL_Disconnect(kvp.Value);
             }
+    #else
+            foreach (var socket in _webSocket.Values)
+                socket?.Disconnect();
+    #endif
 
-            foreach (var socket in _configSo.SocketUris)
+            foreach (var socketCfg in _configSo.SocketUris)
             {
-                var websocket = _webSocket[socket.SocketName] = new(socket.SocketUri, new SocketIOOptions()
-                {
-                    Query = _socketQueries.ToDictionary(x => x.Parameter, x=> x.Value),
-                });
-                
-                websocket.JsonSerializer = new NewtonsoftJsonSerializer();
-                websocket.OnConnected += OnSocketOpen;
-                websocket.OnError += OnSocketError;
-                websocket.OnDisconnected += OnSocketClosed;
-                websocket.OnReconnectAttempt += OnSocketReconnectAttempt;
-                websocket.Connect();
+                var queries = _socketQueries.ToDictionary(x => x.Parameter, x => x.Value);
+                string queryString = string.Join("&", queries.Select(kv => kv.Key + "=" + Uri.EscapeDataString(kv.Value)));
+
+    #if UNITY_WEBGL && !UNITY_EDITOR
+                int instanceId = WebGL_CreateSocket(socketCfg.SocketUri, queryString);
+                _webGLSockets[socketCfg.SocketName] = instanceId;
+                _isConnected[socketCfg.SocketName] = false;
+                _reconnectAttempts[socketCfg.SocketName] = 0;
+    #else
+                var client = new SocketIOUnity(socketCfg.SocketUri, CreateSocketIOOptions());
+                _webSocket[socketCfg.SocketName] = client;
+
+                client.JsonSerializer = new NewtonsoftJsonSerializer();
+                client.OnConnected += OnSocketOpen;
+                client.OnError += OnSocketError;
+                client.OnDisconnected += OnSocketClosed;
+                client.OnReconnectAttempt += OnSocketReconnectAttempt;
+                client.Connect();
+
+                _isConnected[socketCfg.SocketName] = false;
+                _reconnectAttempts[socketCfg.SocketName] = 0;
+    #endif
             }
-
-
         }
 
-        public void Connect(string socketName, List<SocketQueries> socketQueries)
+        public void Connect(string socketName, List<SocketQueries> customQueries)
         {
-            var socket = _configSo.SocketUris.FirstOrDefault(x => x.SocketName == socketName);
-            if (socket == null)
+            var socketCfg = _configSo.SocketUris.FirstOrDefault(x => x.SocketName == socketName);
+            if (socketCfg == null)
             {
-                Debug.LogError($"Found no socket of name: {socketName}");
+                Debug.LogError($"Socket '{socketName}' not found in config");
                 return;
             }
-            if (_webSocket.TryGetValue(socket.SocketName, out var webSocket))
-            {
-                webSocket?.Disconnect();
-            }
-            _webSocket[socketName] = new(socket.SocketUri, new SocketIOOptions()
-            {
-                Query = socketQueries.ToDictionary(x => x.Parameter, x => x.Value),
-            });
-            var websocket = _webSocket[socketName];
-            websocket.JsonSerializer = new NewtonsoftJsonSerializer();
-            websocket.OnConnected += OnSocketOpen;
-            websocket.OnError += OnSocketError;
-            websocket.OnDisconnected += OnSocketClosed;
-            websocket.OnReconnectAttempt += OnSocketReconnectAttempt;
-            websocket.Connect();
-            //Debug.Log(websocket.ServerUri.AbsoluteUri);
+
+            var queries = customQueries.ToDictionary(x => x.Parameter, x => x.Value);
+            string queryString = string.Join("&", queries.Select(kv => kv.Key + "=" + Uri.EscapeDataString(kv.Value)));
+
+    #if UNITY_WEBGL && !UNITY_EDITOR
+            if (_webGLSockets.TryGetValue(socketName, out var oldId))
+                WebGL_Disconnect(oldId);
+
+            int instanceId = WebGL_CreateSocket(socketCfg.SocketUri, queryString);
+            _webGLSockets[socketName] = instanceId;
+            _isConnected[socketName] = false;
+            _reconnectAttempts[socketName] = 0;
+    #else
+            if (_webSocket.TryGetValue(socketName, out var oldSocket))
+                oldSocket?.Disconnect();
+
+            var client = new SocketIOUnity(socketCfg.SocketUri, CreateSocketIOOptions(customQueries));
+            _webSocket[socketName] = client;
+
+            client.JsonSerializer = new NewtonsoftJsonSerializer();
+            client.OnConnected += OnSocketOpen;
+            client.OnError += OnSocketError;
+            client.OnDisconnected += OnSocketClosed;
+            client.OnReconnectAttempt += OnSocketReconnectAttempt;
+            client.Connect();
+
+            _isConnected[socketName] = false;
+            _reconnectAttempts[socketName] = 0;
+    #endif
         }
+
+    #if !(UNITY_WEBGL && !UNITY_EDITOR)
+        private SocketIOOptions CreateSocketIOOptions(List<SocketQueries> customQueries = null)
+        {
+            var queries = (customQueries ?? _socketQueries).ToDictionary(x => x.Parameter, x => x.Value);
+            return new SocketIOOptions()
+            {
+                Query = queries
+            };
+        }
+    #endif
+
         private void OnEnable()
         {
+            Application.focusChanged += OnApplicationFocusChanged;
         }
 
-        private void Start()
+        private void OnApplicationFocusChanged(bool hasFocus)
         {
+            _isApplicationFocused = hasFocus;
+            if (hasFocus)
+            {
+                Debug.Log("Application regained focus - checking socket connections...");
+                CheckAndReconnectSockets();
+            }
+            else
+            {
+                Debug.Log("Application lost focus");
+            }
         }
-
-
 
         private void OnDestroy()
         {
-            foreach (var socket in _webSocket)
+            Application.focusChanged -= OnApplicationFocusChanged;
+    #if UNITY_WEBGL && !UNITY_EDITOR
+            foreach (var kvp in _webGLSockets)
+                WebGL_Disconnect(kvp.Value);
+    #else
+            foreach (var socket in _webSocket.Values)
             {
-                socket.Value?.Disconnect();
-                socket.Value?.Dispose();
+                socket?.Disconnect();
+                socket?.Dispose();
             }
+    #endif
         }
         #endregion
 
         #region MEMBER
-
         public void SubscribeTo(string eventName, string socketName, Action<SocketIOResponse> callback)
         {
-            if (_webSocket.TryGetValue(socketName, out var socket))
+    #if UNITY_WEBGL && !UNITY_EDITOR
+            if (_webGLSockets.TryGetValue(socketName, out var id))
             {
-                socket.On(eventName, callback);
+                WebGL_On(id, eventName);
+                // JS will call back via SendMessage to OnWebGLEvent
             }
+    #else
+            if (_webSocket.TryGetValue(socketName, out var socket))
+                socket.On(eventName, callback);
+    #endif
         }
 
         public async UniTask SendMessage(string eventName, string socketName, string json)
         {
             try
             {
+    #if UNITY_WEBGL && !UNITY_EDITOR
+                if (_webGLSockets.TryGetValue(socketName, out var id))
+                {
+                    if (!_isConnected[socketName])
+                    {
+                        Debug.LogWarning($"WebGL socket '{socketName}' is not connected. Calling connect...");
+                        WebGL_Connect(id);
+                        await UniTask.Delay(TimeSpan.FromSeconds(1));
+                    }
+                    WebGL_Emit(id, eventName, json);
+                }
+    #else
                 if (_webSocket.TryGetValue(socketName, out var socket))
                 {
+                    if (!_isConnected[socketName])
+                    {
+                        Debug.LogWarning($"Socket '{socketName}' is not connected. Attempting to reconnect...");
+                        socket.Connect();
+                        await UniTask.Delay(1000);
+                    }
                     await socket.EmitStringAsJSONAsync(eventName, json);
                 }
+    #endif
                 Debug.Log(eventName);
             }
             catch (Exception e)
             {
-                Debug.LogError(e.Message);
+                Debug.LogError($"Error sending message to {socketName}: {e.Message}");
             }
         }
-
         #endregion
+
         #region INTERNAL
-        private void OnSocketReconnectAttempt(object sender, int e)
+        private void CheckAndReconnectSockets()
         {
-            
+    #if UNITY_WEBGL && !UNITY_EDITOR
+            foreach (var kvp in _webGLSockets)
+            {
+                var name = kvp.Key;
+                if (!_isConnected.ContainsKey(name) || !_isConnected[name])
+                {
+                    Debug.Log($"Reconnecting WebGL socket: {name}");
+                    WebGL_Connect(kvp.Value);
+                }
+            }
+    #else
+            foreach (var kvp in _webSocket)
+            {
+                var name = kvp.Key;
+                var socket = kvp.Value;
+                if (!_isConnected.ContainsKey(name) || !_isConnected[name])
+                {
+                    Debug.Log($"Reconnecting socket: {name}");
+                    socket.Connect();
+                }
+            }
+    #endif
         }
 
-        private void OnSocketOpen(object sender, EventArgs eventArgs)
+    #if UNITY_WEBGL && !UNITY_EDITOR
+        // JavaScript Interop
+        [DllImport("__Internal")]
+        private static extern int WebGL_CreateSocket(string url, string query);
+        [DllImport("__Internal")]
+        private static extern void WebGL_Connect(int instanceId);
+        [DllImport("__Internal")]
+        private static extern void WebGL_Disconnect(int instanceId);
+        [DllImport("__Internal")]
+        private static extern void WebGL_Emit(int instanceId, string eventName, string json);
+        [DllImport("__Internal")]
+        private static extern void WebGL_On(int instanceId, string eventName);
+
+        // Called by JS via SendMessage
+        private void OnWebGLEvent(string message)
         {
-            Debug.Log($"Connection to (websocket: {_configSo.SocketUris.FirstOrDefault().SocketUri}) is Successful!");
+            // Parse message JSON: { instanceId, eventName, data }
+            // Set _isConnected and invoke callbacks or OnSocketOpen/Closed accordingly
+            // TODO: implement parsing and dispatching
+        }
+    #else
+        private void OnSocketOpen(object sender, EventArgs e)
+        {
+            var client = sender as SocketIOUnity;
+            var name = _webSocket.FirstOrDefault(x => x.Value == client).Key;
+            if (name != null)
+            {
+                _isConnected[name] = true;
+                _reconnectAttempts[name] = 0;
+                Debug.Log($"Connection to socket '{name}' successful!");
+            }
             OnSocketConnected?.Invoke();
         }
 
-        private void OnSocketClosed(object sender, string s)
+        private void OnSocketClosed(object sender, string reason)
         {
-            Debug.Log($"Disconnected. {s}");
+            var client = sender as SocketIOUnity;
+            var name = _webSocket.FirstOrDefault(x => x.Value == client).Key;
+            if (name != null)
+            {
+                _isConnected[name] = false;
+                Debug.Log($"Socket '{name}' disconnected. Reason: {reason}");
+                if (_isApplicationFocused && _enableAutoReconnect)
+                    client.Connect();
+            }
         }
 
-        private void OnSocketError(object sender, string s)
+        private void OnSocketError(object sender, string error)
         {
-            Debug.LogError($"Socket error: {s}");
+            var client = sender as SocketIOUnity;
+            var name = _webSocket.FirstOrDefault(x => x.Value == client).Key;
+            Debug.LogError($"Socket error{(name != null ? $" for '{name}'" : "")}: {error}");
         }
+
+        private void OnSocketReconnectAttempt(object sender, int attempt)
+        {
+            var client = sender as SocketIOUnity;
+            var name = _webSocket.FirstOrDefault(x => x.Value == client).Key;
+            if (name != null)
+            {
+                _reconnectAttempts[name] = attempt;
+                Debug.Log($"Reconnection attempt {attempt} for socket '{name}'");
+            }
+        }
+    #endif
         #endregion
     }
 }
